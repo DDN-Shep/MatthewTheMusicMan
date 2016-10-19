84e5afa9f89e79b618328d450ffa4e698b533392
Andrew Sheppard <andrewsheppard@atgmedia.com>
2016-10-19 17:01:09 +0100

    Script fixes, may not keep page.js

--- server/scripts/main.js
+++ server/scripts/main.js
@@ -4,8 +4,8 @@
   var navbar = require('./components/navbar.min.js');
 
   (function initialise() {
-    page('/', load.bind(require('./pages/home.min.js')));
-    page('/todo', load.bind(require('./pages/todo.min.js')));
+    //page('/', load.bind(require('./pages/home.min.js')));
+    //page('/todo', load.bind(require('./pages/todo.min.js')));
     page('*', function() {
       console.log('404', arguments);
     });
